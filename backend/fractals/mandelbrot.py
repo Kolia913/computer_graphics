e973ad6e8c6b963dc665ce4d9388b107e6d4d19b
Roman Skok <rma.skok@gmail.com>
2023-10-22 01:45:23 +0300

    feat: mandblrot fractal

---
+++ backend/fractals/mandelbrot.py
@@ -0,0 +1,118 @@
+import io
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from numba import jit
+
+
+matplotlib.use("agg")
+
+
+@jit(nopython=True)
+def generate_mandelbrot(re: float, im: float, max_iterations: int) -> int:
+    """
+    Generate a Mandelbrot fractal for a given complex coordinate.
+
+    :param re: The real part of the complex coordinate.
+    :param im: The imaginary part of the complex coordinate.
+    :param max_iterations: The maximum number of iterations to perform.
+    :return: The number of iterations required for the sequence to escape the Mandelbrot set.
+    """
+
+    c = complex(re, im)
+    z = 0.0j
+
+    for i in range(max_iterations):
+        z = z * z + c
+
+        if (z.real * z.real + z.imag * z.imag) >= 4:
+            return i
+
+    return max_iterations
+
+
+def generate_mandelbrot_set(zoom_percentage: float, max_iterations: int) -> np.array:
+    """
+    Generate a mandelbrot set with the given zoom percentage and max iterations.
+
+    :param zoom_percentage: The zoom percentage (0 - default image scale, 1 - 100% zoom).
+    :param max_iterations: The maximum number of iterations to perform.
+    :return: A numpy array representing the mandelbrot set.
+    """
+
+    rows, cols = 2000, 2000
+    zoom = 1 + zoom_percentage
+
+    re_range = [-2 / zoom, 1 / zoom]
+    im_range = [-1 / zoom, 1 / zoom]
+
+    re_linspace = np.linspace(re_range[0], re_range[1], num=cols)
+    im_linpace = np.linspace(im_range[0], im_range[1], num=rows)
+
+    result = np.zeros([rows, cols])
+
+    for row_index, re in enumerate(re_linspace):  # real axis
+        for col_index, im in enumerate(im_linpace):  # imaginary axis
+            result[row_index, col_index] = generate_mandelbrot(re, im, max_iterations)
+
+    return result
+
+
+def convert_set_to_image(mandelbrot_set: np.array, color_map: str) -> io.BytesIO:
+    """
+    Convert a mandelbrot set to an image.
+
+    :param mandelbrot_set: A numpy array representing the mandelbrot set.
+    :param color_map: A string representing the color map to be used for the image.
+    :return: A BytesIO object containing the image data.
+    """
+    import matplotlib.pyplot as plt
+
+    plt.figure(dpi=100)
+    plt.imshow(
+        mandelbrot_set.T,
+        cmap=color_map,
+        interpolation="bilinear",
+        extent=[-2, 1, -1, 1],
+    )
+    # hide axis
+    plt.axis("off")
+
+    img_byte_array = io.BytesIO()
+    plt.savefig(img_byte_array, format="png", bbox_inches="tight", pad_inches=0)
+    img_byte_array.seek(0)
+    return img_byte_array
+
+
+def main():
+    cols = 2000
+    rows = 2000
+
+    zoom_percentage = 0
+    zoom = 1 + zoom_percentage
+
+    re_range = [-2 / zoom, 1 / zoom]
+    im_range = [-1 / zoom, 1 / zoom]
+
+    re_linspace = np.linspace(re_range[0], re_range[1], num=cols)
+    im_linpace = np.linspace(im_range[0], im_range[1], num=rows)
+
+    result = np.zeros([rows, cols])
+
+    for row_index, re in enumerate(re_linspace):  # real axis
+        for col_index, im in enumerate(im_linpace):  # imaginary axis
+            result[row_index, col_index] = generate_mandelbrot(re, im, 100)
+
+    # for row_index, re in enumerate(np.linspace(-2, 1, num=rows)):  # real axis
+    #     for col_index, im in enumerate(np.linspace(-1, 1, num=cols)):  # imaginary axis
+    #         result[row_index, col_index] = generate_mandelbrot(re, im, cols, rows, 100)
+
+    plt.figure(dpi=100)
+    plt.imshow(result.T, cmap="hot", interpolation="bilinear", extent=[-2, 1, -1, 1])
+    plt.xlabel("Re")
+    plt.ylabel("Im")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
